d75eab39d291b8c685a7499ac40fc771b4c1cba0
Randall Floyd <rdfloyd@iu.edu>
2025-11-11 14:07:36 -0500

    Adding columns and relations to User for admin features

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_and_belongs_to_many :repositories
+
+  enum role: { manager: 0, admin: 1 }
+  after_initialize :set_default_role, if: :new_record?
+
+  def set_default_role
+    self.role ||= :manager
+  end
+
+  scope :admins, -> { where(role: :admin) }
+
   # Configuration added by Blacklight; Blacklight::User uses a method key on your
   # user class to get a user-displayable login/identifier for
   # the account.
